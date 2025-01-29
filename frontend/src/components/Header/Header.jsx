11b0b7ebb7a36fb68fddc3ba42e569e2387696b0
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-01-29 19:49:42 +0530

    update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Header.css'
 const Header = () => {
+
+  const handleScrollToMenu = () => {
+    document.getElementById('explore-menu').scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div>
       <div className="header">
         <div className="header-contents">
             <h2>Order your favourite food</h2>
             <p>Choose from a diverse menu featuring a delactable array of dishes crafted with the finest ingredients and satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-            <button>View Menu</button>
+            <button onClick={handleScrollToMenu}>View Menu</button>
         </div>
       </div>
     </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart  , getTotalCartAmount} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart  , getTotalCartAmount,url} = useContext(StoreContext);
 
   const navigate = useNavigate();
 
@@ -29,7 +29,7 @@ const Cart = () => {
                   // key={item._id || index}
                   className="cart-items-title cart-items-item"
                 >
-                  <img src={item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   {/* <p>{item.title || 'No title available'}</p> */}
                   <p>${item.price}</p>
