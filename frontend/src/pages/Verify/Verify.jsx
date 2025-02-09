9459ec79522662eb68bb62f513b8091281acfcae
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-02-09 23:54:26 +0530

    update

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,6 +1,46 @@
+// import React, { useEffect } from 'react'
+// import './Verify.css'
+// import { useSearchParams } from 'react-router-dom'
+
+// const Verify = () => {
+
+//     const [searchParams,setSearchParams] = useSearchParams();
+//     const success = searchParams.get("success")
+//     const orderId = searchParams.get("orderId")
+//     const {url} = useContext(StoreContext);
+//     const navigate = useNavigate();
+    
+//     const verifyPayment = async () => {
+//       const response = await axios.post(url+ "/api/order/verify" , {success,orderId});
+//       if(response.data.success){
+//         navigate("/myorders");
+//       }
+//       else{
+//         navigate("/")
+//       }
+//     }
+
+//     useEffect(() =>{
+//       verifyPayment();
+//     },[])
+
+//   return (
+//     <div className='verify'>
+//       <div className="spinner"></div>
+      
+//     </div>
+//   )
+// }
+
+// export default Verify
+
+
 import React, { useEffect } from 'react'
 import './Verify.css'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const Verify = () => {
 
@@ -9,25 +49,25 @@ const Verify = () => {
     const orderId = searchParams.get("orderId")
     const {url} = useContext(StoreContext);
     const navigate = useNavigate();
-    
+
     const verifyPayment = async () => {
-      const response = await axios.post(url+ "/api/order/verify" , {success,orderId});
-      if(response.data.success){
-        navigate("/myorders");
-      }
-      else{
-        navigate("/")
-      }
+        const response = await axios.post(url+"/api/order/verify",{success,orderId});
+        if (response.data.success){
+            navigate("/myorders");
+        }
+        else {
+            navigate("/")
+        }
     }
 
-    useEffect(() =>{
-      verifyPayment();
+
+    useEffect(()=>{
+        verifyPayment();
     },[])
 
   return (
     <div className='verify'>
-      <div className="spinner"></div>
-      
+        <div className="spinner"></div>
     </div>
   )
 }
